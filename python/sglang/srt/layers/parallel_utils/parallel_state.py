e43d9920cecd324730b3804d2ac2373e4942f282
Yifan Qiao <yifanqiao@ucla.edu>
2024-07-20 23:18:34 -0700

    fix [parallel_utils]: add missed files

---
+++ python/sglang/srt/layers/parallel_utils/parallel_state.py
@@ -0,0 +1,79 @@
+from vllm.distributed import initialize_model_parallel as vllm_initialize_model_parallel
+from vllm.distributed.parallel_state import (
+    get_tensor_model_parallel_group,
+    get_tensor_model_parallel_rank,
+)
+
+_SEQUENCE_PARALLEL_SIZE = None
+
+
+def initialize_model_parallel(
+    tensor_model_parallel_size: int = 1,
+    pipeline_model_parallel_size: int = 1,
+    sequence_parallel_size: int = 1,
+) -> None:
+    global _SEQUENCE_PARALLEL_SIZE
+    assert _SEQUENCE_PARALLEL_SIZE is None
+    _SEQUENCE_PARALLEL_SIZE = sequence_parallel_size
+    vllm_initialize_model_parallel(
+        tensor_model_parallel_size, pipeline_model_parallel_size
+    )
+
+
+def sequence_parallel_is_initialized():
+    return _SEQUENCE_PARALLEL_SIZE is not None
+
+
+def get_sequence_parallel_world_size():
+    assert _SEQUENCE_PARALLEL_SIZE is not None
+    return _SEQUENCE_PARALLEL_SIZE
+
+
+def get_sequence_parallel_local_rank(rank: int = None):
+    assert _SEQUENCE_PARALLEL_SIZE is not None
+    if rank is None:
+        rank = get_tensor_model_parallel_rank()
+    local_rank = rank % _SEQUENCE_PARALLEL_SIZE
+    return local_rank
+
+
+def get_sequence_parallel_global_rank():
+    return get_tensor_model_parallel_rank()
+
+
+def get_sequence_parallel_first_rank(rank: int = None):
+    assert _SEQUENCE_PARALLEL_SIZE is not None
+    if rank is None:
+        rank = get_tensor_model_parallel_rank()
+    first_rank = rank // _SEQUENCE_PARALLEL_SIZE * _SEQUENCE_PARALLEL_SIZE
+    return first_rank
+
+
+def get_sequence_parallel_last_rank(rank: int = None):
+    assert _SEQUENCE_PARALLEL_SIZE is not None
+    if rank is None:
+        rank = get_tensor_model_parallel_rank()
+    last_rank = (
+        rank // _SEQUENCE_PARALLEL_SIZE * _SEQUENCE_PARALLEL_SIZE
+        + _SEQUENCE_PARALLEL_SIZE
+        - 1
+    )
+    return last_rank
+
+
+def get_sequence_parallel_next_rank(rank: int = None):
+    assert _SEQUENCE_PARALLEL_SIZE is not None
+    if rank is None:
+        rank = get_tensor_model_parallel_rank()
+    first_rank = get_sequence_parallel_first_rank(rank)
+    next_rank = first_rank + (rank + 1) % _SEQUENCE_PARALLEL_SIZE
+    return next_rank
+
+
+def get_sequence_parallel_prev_rank(rank: int = None):
+    assert _SEQUENCE_PARALLEL_SIZE is not None
+    if rank is None:
+        rank = get_tensor_model_parallel_rank()
+    first_rank = get_sequence_parallel_first_rank(rank)
+    prev_rank = first_rank + (rank - 1) % _SEQUENCE_PARALLEL_SIZE
+    return prev_rank
